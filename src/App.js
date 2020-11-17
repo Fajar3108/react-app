ec7f4eeb341ad3c69e8670070f935f1741cf023c
Fajar3108 <maulanafajaribrahim@gmail.com>
2020-11-17 12:19:57 +0700

    Belajar Membuat Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  let nama = "Maulana Fajar Ibrahim";
-  let kelas = "XI RPL";
-  let absen = "15";
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="App-nama">{nama}</h1>
-        <h2 className="App-detail">{kelas} | {absen}</h2>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 
+function App(props) {
+  return (
+    <div className="App">
+      <h1>{props.nama}</h1>
+      <h2>{props.kelas} | {props.absen}</h2>
+    </div>
+  );
+}
+
+class AppClass extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      nama: "Maulana Fajar Ibrahim",
+      kelas: "XI RPL",
+      absen: 15 
+    };
+  }
+  render(){
+    return(
+      <div>
+        <h1>{this.state.nama}</h1>
+        <h2>{this.state.kelas} | {this.state.absen}</h2>
+      </div>
+    );
+  }
+}
+function Bio(props) {
+  return (
+    <div className="App">
+      <h1>{props.nama}</h1>
+      <h2>{props.kelas} | {props.absen}</h2>
+    </div>
+  );
+}
+const AppVar = <Bio nama="Maulana Fajar Ibrahim" kelas="XI RPL" absen="15" />;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App nama="Maulana Fajar Ibrahim" kelas="XI RPL" absen="15" />,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
